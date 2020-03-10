5037486de93618586140f35756d2af374300ac63
mat8man <61152696+mat8man@users.noreply.github.com>
2020-03-10 14:47:45 +0100

    Update MatteosShit.m

--- MatteosShit.m
+++ MatteosShit.m
@@ -13,22 +13,25 @@ run("Cit_par.m");
 p = 1
 p0 = 1
 gamma = 1.4
-hp = 1
-g0 = 1
+
+alt = flightdata.Dadc1_alt.data
+hp = alt
+
 cas = flightdata.Dadc1_cas.data
 V_c = cas
-Tm = 1
+sat = flightdata.Dadc1_sat.data
+Tm = sat
 
 %P
-p = p0* ( (1 + ((lambda*hp)/Temp0))^(-(g0/(lambda*R))))
+p = p0* ( (1 + ((lambda*hp)/Temp0))^(-(g/(lambda*R))));
 %M
-M = sqrt(    (2/(gamma-1)) * ((1+(p0/p)*((1+((gamma-1)/(2*gamma))*(rho0/p0)*V_c^2)^(gamma/(gamma-1))-1))^(((gamma-1)/gamma) -1)   )
+M = sqrt(    (2/(gamma-1)) * ((1+(p0/p)*((1+((gamma-1)/(2*gamma))*(rho0/p0)*V_c^2)^(gamma/(gamma-1))-1))^(((gamma-1)/gamma) -1)   );
 %T 
-T = Tm/(1+((gamma-1)/2)*M^2)
+T = Tm/(1+((gamma-1)/2)*M^2);
 %a
-a = sqrt( gamma*R*T)
+a = sqrt( gamma*R*T);
 %V_t
-V_t = M*a
+V_t = M*a;
 %V_e
 V_e = V_t * sqrt((rho/rho0))
 
