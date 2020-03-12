8f6f3ce16bcf066e8243accf5076487e49df23fb
mat8man <61152696+mat8man@users.noreply.github.com>
2020-03-12 17:21:00 +0100

    Update MatteosShit.m

--- MatteosShit.m
+++ MatteosShit.m
@@ -9,14 +9,16 @@
 load('matlab.mat')
 % load('Cit_par.m')
 
-output = equivalentAirspeed(20009, lambda, Temp0, g, R, flightdata);
+output = eas(4600, lambda, Temp0, g, R, flightdata, pounds_ZFM,pounds_FuelStart,Wlh,Wrh, time);
 
-function [V_e]= equivalentAirspeed(t, lambda, Temp0, g, R, flightdata)
+function [V_e]= eas(t, lambda, Temp0, g, R, flightdata, pounds_ZFM,pounds_FuelStart,Wlh,Wrh, time)
 %This function calculates the Equivalent Aispeed
 %Constants:
 run("Cit_par.m");
 p0 = 101325;
 gamma = 1.4;
+W_s = 60500; %N
+t_0 = 9;
 %Inputs:
 alt = flightdata.Dadc1_alt.data;
 hp = alt.*0.3048;
@@ -25,18 +27,27 @@ V_c = cas.*0.514444;
 sat = flightdata.Dadc1_sat.data;
 Tm = sat + 273.15;
 
-hp_t = hp(t);
-V_c_t = V_c(t);
-Tm_t = Tm(t);
+%tHZ = 10*(t-t_0) + 1;
+tHZ = find(time==t);
+tHZ
+
+hp_t = hp(tHZ);
+V_c_t = V_c(tHZ);
+Tm_t = Tm(tHZ);
+
+
+
+W1=W(t,pounds_ZFM,pounds_FuelStart,Wlh,Wrh);
+W2=W1(2);
 
 
 %P 
 p = p0* ( (1 + ((lambda*hp_t)/Temp0)).^(-(g/(lambda*R))));
 %M
-M025 = 1 + ((gamma-1)/2*gamma) * (rho0/p0) * V_c_t.^2; 
+M025 = 1 + ((gamma-1)/(2*gamma)) * (rho0/p0) * V_c_t.^2; 
 M05 = M025.^(gamma/(gamma-1));
 M1 = M05 - 1;
-M2 = 1 + (p0./p).*M1; % HERE SOMETHIMG GOES WRONG
+M2 = 1 + (p0./p).*M1; 
 M3 = M2.^((gamma-1)/gamma);
 M4 = M3 - 1 ;
 M5 = (2/(gamma-1))*M4 ;
@@ -52,9 +63,8 @@ rhoreduction1 = R .* T;
 rhoreduction2 = p.* (1./rhoreduction1);
 %V_e
 V_e = V_t .* sqrt((rhoreduction2./rho0))
+V_e_bar = V_e * sqrt(W_s/W2)
 end
-
-
 
 
  
