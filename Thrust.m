8327581fc417b57793fe265e39d84083e4417924
Yuri133 <yuri.laar@gmail.com>
2020-03-12 18:23:39 +0100

    Update Thrust.m

--- Thrust.m
+++ Thrust.m
@@ -1,19 +1,22 @@
 %input
-Hp        = 2060 %m
-Mach      = 0.50
-fuelleft  = 0.0400 %kg/s
-fuelright = 0.0300 %kg/s
-Delta_T   =  5    %degrees
+Hp        = 2060; %m
+Mach      = 0.50;
+fuelleft  = 0.0400; %kg/s
+fuelright = 0.0300;%kg/s
+Delta_T   =  5;    %degrees
 
 %writing to file
-array= [Hp, Mach,Delta_T,fuelleft,fuelright]
+array= [Hp, Mach,Delta_T,fuelleft,fuelright];
 fileID = fopen('matlab.dat','w');
 for i = 1:1:length(array);
-fprintf(fileID,"%d\n",array(i))
+fprintf(fileID,"%d\n",array(i));
 end
 fclose(fileID);
 
-%output
-system('thrust.exe &')
 
-%Results of thrust right and thrust left engine are in thrust.dat
+system('thrust.exe &');
+
+%output
+load("thrust.dat");
+Thrustlr = thrust
+
