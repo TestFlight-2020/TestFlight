d764c0e1a6cfbefa6d491d4504434ec2492736b2
Yuri133 <yuri.laar@gmail.com>
2020-03-12 18:17:30 +0100

    j

--- Thrust.m
+++ Thrust.m
@@ -1,12 +1,16 @@
-Hp        = 6060 %ft
-Mach      = 10
-fuelleft  = 400
-fuelright = 300
-Delta_T   =  10
-array= [Hp, Mach,fuelleft,fuelright,Delta_T]
+Hp        = 2060 %m
+Mach      = 0.50
+fuelleft  = 0.0400
+fuelright = 0.0300
+Delta_T   =  5
+array= [Hp, Mach,Delta_T,fuelleft,fuelright]
 
 fileID = fopen('matlab.dat','w');
 for i = 1:1:length(array);
 fprintf(fileID,"%d\n",array(i))
 end
 fclose(fileID);
+
+system('thrust.exe &')
+
+%Results of thrust right and thrust left engine are in thrust.dat
